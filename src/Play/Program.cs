fc55e3518ac99516348d64d8e02e34b48c735e77
Andrew <andrewmjordan@users.noreply.github.com>
2021-10-22 08:13:36 -0700

    complete preliminary program

--- src/Play/Program.cs
+++ src/Play/Program.cs
@@ -1,65 +1,85 @@
 ﻿using CaseExtensions;
+using CliWrap;
 using F23.StringSimilarity;
+using FuzzySharp;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-class Program
+namespace Andtech
 {
-	static List<string> ValidExtensions = new List<string>()
+
+	class Program
 	{
-		".mp3",
-		".mp4",
-		".wav",
-		".aiff",
-		".flac"
-	};
 
-	class FileData
-	{
-		public string Path { get; set; }
-		public double Score { get; set; }
-	}
-
-	static void Main(string[] args)
-	{
-		var root = ".";
-		var query = string.Join(" ", args);
-		query = query.ToKebabCase();
-
-		Console.WriteLine($"'{query}'");
-		var l = new MetricLCS();
-
-		var files = Directory
-			.EnumerateFiles(root, "*", SearchOption.AllDirectories)
-			.Where(IsMusicFile);
-		var datas = files
-			.Select(x => new FileData { Path = x, Score = GetScore(x) });
-
-		double GetScore(string path)
+		class FileData
 		{
-			var filename = Path.GetFileNameWithoutExtension(path);
-			var distance = l.Distance(query, filename);
-
-			return distance;
+			public string Path { get; set; }
+			public string Term { get; set; }
+			public double Score { get; set; }
 		}
 
-		var top = datas.OrderBy(x => x.Score).Take(10);
-		var best = top.First();
-
-		foreach (var element in top)
+		static async Task Main(string[] args)
 		{
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine($"{element.Path} ({element.Score})");
+			var root = ".";
+			var query = Standardize(string.Join(" ", args));
+
+			var l = new MetricLCS();
+
+			var terms = args.Select(x => $@"\b{x}[^\s]*");
+			var regex = new Regex($"{string.Join(@"\s+([^\s]+\s+)*", terms)}");
+			Console.WriteLine(regex);
+
+			var searcher = new Searcher();
+			var files = searcher.EnumerateFiles(root);
+			var datas = files
+				.Select(ToFileData);
+			Console.WriteLine($"'{query}'");
+
+			FileData ToFileData(string path)
+			{
+				var filename = Path.GetFileNameWithoutExtension(path);
+				var term = Standardize(filename);
+
+				return new FileData { Path = path, Term = term, Score = Fuzz.PartialTokenSetRatio(query, term, FuzzySharp.PreProcess.PreprocessMode.Full) };
+			}
+
+			datas = datas.Where(x => regex.IsMatch(x.Term));
+
+			var top = datas.OrderByDescending(x => x.Score).Take(6);
+
+			foreach (var element in datas.OrderByDescending(x => x.Score).Take(6))
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine($"{element.Term} ({element.Score})");
+			}
+
+			if (top.Any())
+			{
+				var best = top.First();
+				Console.ForegroundColor = ConsoleColor.Green;
+				var player = Environment.GetEnvironmentVariable("PLAYER");
+				Console.WriteLine($"{player} {best.Path}");
+
+				return;
+				await Cli.Wrap(player)
+					.WithArguments(best.Path)
+					.ExecuteAsync();
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.Error.WriteLine($"No matches");
+			}
 		}
 
-		Console.ForegroundColor = ConsoleColor.Cyan;
-		Console.WriteLine($"{best.Path} ({best.Score})");
-	}
-
-	static bool IsMusicFile(string path)
-	{
-		return ValidExtensions.Contains(Path.GetExtension(path));
+		static string Standardize(string x)
+		{
+			x = x.ToKebabCase();
+			x = x.Replace("-", " ");
+			return x;
+		}
 	}
 }
---
+++ src/Play/Searcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Andtech
+{
+	class Searcher
+	{
+		static List<string> ValidExtensions = new List<string>()
+		{
+			".mp3",
+			".mp4",
+			".wav",
+			".aiff",
+			".flac"
+		};
+
+		public IEnumerable<string> EnumerateFiles(string root)
+		{
+			return Directory
+				.EnumerateFiles(root, "*", SearchOption.AllDirectories)
+				.Where(IsMusicFile);
+		}
+
+		static bool IsMusicFile(string path)
+		{
+			return ValidExtensions.Contains(Path.GetExtension(path));
+		}
+	}
+}
