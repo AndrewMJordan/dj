e0af4ce7da4c59fa0831502836d53c98294f0a31
Andrew <andrewmjordan@users.noreply.github.com>
2021-11-20 01:53:47 -0800

    implement natural cli parsing

--- src/DJ/Models/Query.cs
+++ src/DJ/Models/Query.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Andtech.Models
@@ -18,13 +19,60 @@ namespace Andtech.Models
         {
             var input = string.Join(" ", tokens.Select(x => x.Trim()));
 
+            int n = tokens.Length;
+
+            bool hasTitle = !string.IsNullOrEmpty(title);
+            bool hasArtist = !string.IsNullOrEmpty(artist);
+            bool hasAlbum = !string.IsNullOrEmpty(album);
+
+            bool foundArtistSplit = TryLastIndexOf(tokens, "by", out var indexOfArtistSplit, 0);
+            bool foundAlbumSplit = TryLastIndexOf(tokens, "from", out var indexOfAlbumSplit, foundArtistSplit ? indexOfArtistSplit + 1 : 0);
+            if (hasArtist)
+			{
+                indexOfArtistSplit = n;
+			}
+            if (hasAlbum)
+			{
+                indexOfAlbumSplit = n;
+			}
+            indexOfArtistSplit = Math.Min(indexOfArtistSplit, indexOfAlbumSplit);
+            indexOfAlbumSplit = Math.Max(indexOfArtistSplit, indexOfAlbumSplit);
+
+            if (!hasTitle)
+			{
+                title = string.Join(" ", tokens.Take(indexOfArtistSplit));
+            }
+            if (!hasArtist)
+            {
+                artist = string.Join(" ", tokens.Skip(indexOfArtistSplit + 1).Take(indexOfAlbumSplit - indexOfArtistSplit - 1));
+            }
+            if (!hasAlbum)
+            {
+                album = string.Join(" ", tokens.Skip(indexOfAlbumSplit + 1).Take(n - indexOfAlbumSplit));
+            }
+
             return new Query()
             {
-                Title = string.IsNullOrEmpty(title) ? Utility.Standardize(input) : title,
+                Title = Utility.Standardize(title),
                 Artist = Utility.Standardize(artist),
                 Album = Utility.Standardize(album),
                 Raw = input
             };
+        }
+
+        public static bool TryLastIndexOf(string[] tokens, string keyword, out int index, int lowerBound = 0)
+        {
+            for (index = tokens.Length - 2; index >= lowerBound + 1; index--)
+            {
+                if (string.Equals(tokens[index], keyword, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            index = tokens.Length;
+
+            return false;
         }
     }
 }
