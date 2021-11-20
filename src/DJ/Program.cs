cff547577a029deb326d687d6ffee02beeac695c
Andrew <andrewmjordan@users.noreply.github.com>
2021-11-19 21:38:54 -0800

    wip: organize classes

--- src/DJ/Program.cs
+++ src/DJ/Program.cs
@@ -12,78 +12,38 @@ using System.Threading.Tasks;
 namespace Andtech
 {
 
+	public class Options
+	{
+		[Value(0)]
+		public IList<string> Tokens { get; set; }
+
+		[Option('l', "list", HelpText = "List search results instead of enqueuing them.")]
+		public bool List { get; set; }
+
+		[Option("verbose", HelpText = "Print verbose messages.")]
+		public bool Verbose { get; set; }
+	}
+
+
 	class Program
 	{
 
-		[Verb("play", isDefault: true, HelpText = "Play the music file.")]
-		class PlayOptions
-		{
-			[Value(0)]
-			public IList<string> Tokens { get; set; }
-
-			[Option('l', "list", HelpText = "List search results instead of enqueuing them.")]
-			public bool List { get; set; }
-		}
-
 		static async Task Main(string[] args)
 		{
-			var result = Parser.Default.ParseArguments<PlayOptions>(args);
-			await result.WithParsedAsync(Play);
+			var result = Parser.Default.ParseArguments<Options>(args);
+			await result.WithParsedAsync(OnParse);
 		}
 
-		static async Task Play(PlayOptions options)
+		public static async Task OnParse(Options options)
 		{
+			var runner = new Runner(options);
 			if (options.List)
 			{
-				List(options.Tokens.ToArray());
+				await runner.List();
 			}
 			else
 			{
-				var musicDir = Environment.GetEnvironmentVariable("XDG_MUSIC_DIR");
-				musicDir = Directory.Exists(musicDir) ? musicDir : Environment.CurrentDirectory;
-
-				var searcher = new AudioFileSearcher(musicDir);
-				var results = searcher.GetRanking(options.Tokens.ToArray());
-
-				if (results.Any())
-				{
-					var best = results.OrderByDescending(x => x.Score).First();
-
-					var player = Environment.GetEnvironmentVariable("PLAYER");
-					var process = new AudioPlayerProcess(player);
-
-					var audioFile = new AudioFile()
-                    {
-						Path = best.Path,
-						Title = best.Term.Humanize(LetterCasing.Title),
-						Artist = "Unknown Artist"
-                    };
-                    process.Play(audioFile);
-				}
-				else
-				{
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.Error.WriteLine($"No matches");
-				}
-			}
-		}
-
-		static void List(string[] args)
-		{
-			var searcher = new AudioFileSearcher();
-			var results = searcher.GetRanking(args);
-
-			if (results.Any())
-			{
-				foreach (var result in results.OrderByDescending(x => x.Score).Take(5))
-				{
-					Console.WriteLine($"{result.Score}\t{result.Term.Transform(To.TitleCase)}");
-				}
-			}
-			else
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.Error.WriteLine($"No matches");
+				await runner.Play();
 			}
 		}
 	}
