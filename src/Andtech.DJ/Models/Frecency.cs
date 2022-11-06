77ad6fd98c1f16e2d586d5040af226d1dbe23aed
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-11-05 19:17:14 -0700

    fix: fix frecency calculation

--- src/Andtech.DJ/Models/Frecency.cs
+++ src/Andtech.DJ/Models/Frecency.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Andtech.Common;
+using System;
 
 namespace Andtech.DJ.Utility
 {
@@ -7,7 +8,7 @@ namespace Andtech.DJ.Utility
 	{
 		public readonly double HalfLife;
 		public readonly double DecayFactor;
-		public readonly double DefaultBaseBonus = 1.0;
+		public readonly double DefaultBaseBonus = 1;
 
 		public Frecency(double halfLife)
 		{
@@ -17,16 +18,20 @@ namespace Andtech.DJ.Utility
 
 		public DateTime IncreaseScore(DateTime criticalDate)
 		{
-			var tau = Math.Abs((DateTime.UtcNow - criticalDate).TotalDays);
+			var tau = (DateTime.UtcNow - criticalDate.ToUniversalTime()).TotalDays;
 			var score = Math.Exp(-DecayFactor * tau);
+			Log.WriteLine($"Old score: {score}", ConsoleColor.Blue, Verbosity.silly);
+			Log.WriteLine($"Old tau: {tau}", ConsoleColor.Blue, Verbosity.silly);
 			var bonus = DefaultBaseBonus;
+			Log.WriteLine($"New score: {score + DefaultBaseBonus}", ConsoleColor.Blue, Verbosity.silly);
 			var nextTau = Math.Log(score + bonus) / DecayFactor;
+			Log.WriteLine($"New tau: {nextTau}", ConsoleColor.Blue, Verbosity.silly);
 			return DateTime.UtcNow + TimeSpan.FromDays(nextTau);
 		}
 
 		public double Decode(DateTime criticalDate)
 		{
-			var tau = (DateTime.UtcNow - criticalDate).TotalDays;
+			var tau = (DateTime.UtcNow - criticalDate.ToUniversalTime()).TotalDays;
 			return Math.Exp(-DecayFactor * tau);
 		}
 	}
